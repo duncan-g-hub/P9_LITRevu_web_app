a15c61c28a7546eeb8fbf5478f42cfd7e123538f
duncan-g-hub <duncan.gaurat@outlook.fr>
2026-03-17 09:53:05 +0100

    Ajout model user à la base de donnée

--- LITRevu/authentication/models.py
+++ LITRevu/authentication/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+class User(AbstractUser):
+    # modele user de base, on laisse la possiblité d'ajouter des champs supplémentaire avec AbstractUser)
+    pass
